8a52d980cbcfec91e2bf65866ca30063f7dc088d
sumo300 <msumerano@gmail.com>
2023-12-04 08:03:16 +0000

    AU: 1 updated - mediaelch [skip ci] https://gist.github.com/sumo300/4f822ef7bbf7188cd0b108ed8c6f7e84/29d735795df83bc3e85fcdb763d7423f4609c1c4

--- mediaelch/tools/chocolateyInstall.ps1
+++ mediaelch/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
   packageName    = 'MediaElch'
-  url            = 'https://github.com/Komet/MediaElch/releases/download/v2.10.4/MediaElch_win_10_or_later_Qt6_2.10.4_2023-07-30_git-0722e4ee.zip'
-  checksum       = '631865147f5b20f43a90f597b4c540338d9cf5938a729e185246879957932cc3'
+  url            = 'https://github.com/Komet/MediaElch/releases/download/v2.10.6/MediaElch_win_10_or_later_Qt6_2.10.6_2023-12-03_git-6121419e.zip'
+  checksum       = '0c6061e1768867729efbf3fd368320886898fc50d42b7bbf66fa172e274b4bb7'
   checksumType   = 'sha256'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 }
